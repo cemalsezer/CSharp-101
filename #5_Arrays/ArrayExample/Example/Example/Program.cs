09d22813a992f1c3bbad41d9b28e80a6fa374481
cemal sezer <cemalszr35@gmail.com>
2022-01-21 18:13:11 +0300

    arrays added

---
+++ #5_Arrays/ArrayExample/Example/Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç boyutlu dizi olsun: ");
+            int cevap = int.Parse(Console.ReadLine());
+            var rnd = new Random();
+
+            int[] dizi = new int[cevap];
+
+            for (int i = 0; i < dizi.Length; i++)
+                dizi[i] = rnd.Next(1, 59);
+
+            foreach (var cikti in dizi)
+            {
+                Console.WriteLine(cikti);
+            }
+        }
+    }
+}
