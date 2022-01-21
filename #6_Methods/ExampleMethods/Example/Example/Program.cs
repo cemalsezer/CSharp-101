9f9206cfa661991a8799fd6ba126755d11ed7a79
cemal sezer <cemalszr35@gmail.com>
2022-01-21 18:13:36 +0300

    added

---
+++ #6_Methods/ExampleMethods/Example/Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Total create = new Total();
+
+            Console.WriteLine(create.toplam(5, 3)); 
+
+
+        }
+
+
+        
+    }
+   
+}
---
+++ #6_Methods/ExampleMethods/Example/Example/Total.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Total
+    {
+        public int  toplam(int n1, int n2)
+        {
+            int total = n1 + n2;
+            return total;
+        }
+    }
+}
