28caa9327b8d313290cd5a79f1bc34b52f649abe
cemal sezer <cemalszr35@gmail.com>
2022-01-21 17:58:37 +0300

    while added

---
+++ #4_Loops/WhileExample/WhileExamples/WhileExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WhileExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi = 0;
+            //while(sayi<10)
+            //{
+            //    sayi++;
+            //    Console.WriteLine(sayi);
+
+            //}
+
+            //int n1 = 0;
+            //int sum = 0;
+            
+
+            //while(n1<100)
+            //{
+                
+            //    sum += n1;
+            //    n1++;
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+        }
+    }
+}
