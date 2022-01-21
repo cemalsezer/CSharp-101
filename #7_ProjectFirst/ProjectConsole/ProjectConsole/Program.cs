11eab8c71980c1e9a2844caffe2c861c5ed069f7
cemal sezer <cemalszr35@gmail.com>
2022-01-21 18:35:16 +0300

    ProjectOne Added

---
+++ #7_ProjectFirst/ProjectConsole/ProjectConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ProjectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dongu;
+            int cevap;
+            Console.Write("Döngü sayısını giriniz: ");
+            dongu = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= dongu; i++)
+            {
+                Console.Write("{0}.Sayıyı Giriniz: ",i);
+                 cevap = Convert.ToInt32(Console.ReadLine());
+                kontrol(cevap);
+                
+            }
+        }
+        static void kontrol(int s)
+        {
+            if (s % 2 == 0)
+            {
+                Console.WriteLine("Girilen Sayı Çift");
+            }
+            else
+            {
+                Console.WriteLine("Girilen Sayı Tek");
+            }
+        }
+    }
+}
